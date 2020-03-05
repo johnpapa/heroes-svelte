5069dd3b0598693016033f9e12fc862801ba5b9e
John Papa <john@johnpapa.net>
2020-03-05 15:26:15 -0500

    what????

--- src/villains/VillainList.svelte
+++ src/villains/VillainList.svelte
@@ -1,16 +1,16 @@
 <script>
-  import {createEventDispatcher} from 'svelte';
-  import {ButtonFooter, CardContent} from '../components';
+  import { createEventDispatcher } from 'svelte';
+  import { ButtonFooter, CardContent } from '../components';
 
-  const dispatch = createEventDispatcher ();
+  const dispatch = createEventDispatcher();
   export let villains = [];
 
-  function deleteVillain (villain) {
-    dispatch ('deleted', villain);
+  function deleteVillain(villain) {
+    dispatch('deleted', villain);
   }
 
-  function selectVillain (villain) {
-    dispatch ('selected', villain);
+  function selectVillain(villain) {
+    dispatch('selected', villain);
   }
 
   const deleteOptions = {
@@ -26,26 +26,28 @@
   };
 </script>
 
-<ul class = "list">
-  {#each villains as {id, name, description}, i (id)}
-    <li role = "presentation">
-      <div class = "card">
+<ul class="list">
+  {#each villains as { id, name, description }, i (id)}
+    <li role="presentation">
+      <div class="card">
         <CardContent {name} {description} />
-        <footer class = "card-footer">
+        <footer class="card-footer">
           <ButtonFooter
-            {... deleteOptions}
-            dataId = {id}
-            dataIndex = {i}
-            item = {villains [i]}
-            on: clicked = {deleteVillain (villains [i])} />
+            {...deleteOptions}
+            dataId={id}
+            dataIndex={i}
+            item={villains[i]}
+            on:
+            clicked={deleteVillain(villains[i])} />
           <ButtonFooter
-            {... editOptions}
-            dataId = {id}
-            dataIndex = {i}
-            item = {villains [i]}
-            on: clicked = {selectVillain (villains [i])} />
+            {...editOptions}
+            dataId={id}
+            dataIndex={i}
+            item={villains[i]}
+            on:
+            clicked={selectVillain(villains[i])} />
         </footer>
       </div>
     </li>
-  {/ each}
-</ Ul>
+  {/each}
+</ul>
